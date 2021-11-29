814c75b805f100ac00699d7d017a7ead22161a39
BNorasingh <90635995+BNorasingh@users.noreply.github.com>
2021-11-29 15:52:48 -0500

    Create ipenabled.ps1

---
+++ powershell/ipenabled.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "Network Displayed:"
+
+get-ciminstance win32_networkadapterconfiguration | where-object ipenabled |
+select-object Description, index,
+@{n="Subnet";e={$_.IPSubnet}},
+@{n="DNS Domain";e={$_.DNSDomain}},
+@{n="DNS Server";e={$_.DNSServerSearchOrder}},
+@{n="IPAddresses";e={$_.IPaddress}} | format-table
